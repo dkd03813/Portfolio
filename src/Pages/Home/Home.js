5453cd918027735b4d63fc1ba5c2c3caebdb1a97
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-26 13:24:40 -0400

    Made a change to my resume

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
         <div className='bg-homeBg min-h-screen  dark:bg-homeBg-dark bg-no-repeat bg-center bg-cover bg-fixed  md:pb-16 w-full'>
             {/* Header Call here */}
             <Header />
+            
         </div>
     );
 };
